5254566d9eba2fd39d0beb0fe753a38e0653f2d3
Aditya Tyagi <adityatyagi20bcs126@iiitkottayam.ac.in>
2023-09-20 15:36:44 +0530

    Initial Commit

---
+++ Cryptic/CrypticApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypticApp.swift
+//  Cryptic
+//
+//  Created by Aditya Tyagi  on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CrypticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
